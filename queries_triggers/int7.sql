dec10611ea83281b70e8dd7d4821e0cdf6b6f1c2
thePeras <jtopereira@gmail.com>
2022-12-19 15:07:06 +0000

    estatisticas de um jogo
    Co-authored-by: gFreirex101 <guilodinho@gmail.com>

--- queries_triggers/int7.sql
+++ queries_triggers/int7.sql
@@ -4,3 +4,62 @@
 .nullvalue NULL
 */
 
+--- Estatísticas de uma equipa numa época (número de golos, número de cada tipo de interrupção)
+
+DROP VIEW IF EXISTS interrupções;
+CREATE VIEW interrupções AS
+SELECT Epoca.inicio as epoca, Equipa.nome equipa, Interrupcao.tipo
+FROM Interrupcao
+JOIN InscricaoAtleta ON Interrupcao.atleta = InscricaoAtleta.atleta
+JOIN Epoca ON Epoca.inicio = InscricaoAtleta.epoca
+JOIN Equipa ON Equipa.id = InscricaoAtleta.equipa;
+
+
+SELECT 
+    DISTINCT epocaInício, 
+    epocaFim, 
+    nomeEquipa, 
+    Golos, 
+    Infrações, 
+    Expulsões, 
+    Advertências, 
+    Exclusões, 
+    Desqualificações
+FROM 
+
+(SELECT Epoca.inicio as epocaInício, Epoca.fim as epocaFim, Equipa.nome as nomeEquipa, Count(*) as Golos 
+from Equipa, Epoca, InscricaoAtleta, Golo
+where Golo.atleta = InscricaoAtleta.atleta and Equipa.id = InscricaoAtleta.equipa and Epoca.inicio = InscricaoAtleta.epoca
+group by Epoca,Equipa.id
+) as gol,  
+  
+(SELECT equipa, Count(*) as Expulsões from interrupções
+where tipo = 'Expulsão'
+group by epoca, equipa
+) as ex, 
+
+(SELECT equipa, Count(*) as Infrações from interrupções
+where tipo = 'Infração'
+group by epoca, equipa
+) as inf, 
+
+(SELECT equipa, Count(*) as Advertências from interrupções
+where tipo = 'Advertência'
+group by epoca, equipa
+) as adv, 
+
+(SELECT equipa, Count(*) as Exclusões from interrupções
+where tipo = 'Exclusão'
+group by epoca, equipa
+) as exc, 
+
+(SELECT equipa, Count(*) as Desqualificações from interrupções
+where tipo = 'Desqualificação'
+group by epoca, equipa
+) as desq
+
+where ex.equipa = nomeEquipa
+and inf.equipa = nomeEquipa 
+and adv.equipa = nomeEquipa 
+and exc.equipa = nomeEquipa
+and desq.equipa = nomeEquipa;
