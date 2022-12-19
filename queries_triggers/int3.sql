7b3bc0cb13a2aaaeee80a0892b587bb097b581b0
thePeras <jtopereira@gmail.com>
2022-12-19 16:07:35 +0000

    timeline de um jogo query

--- queries_triggers/int3.sql
+++ queries_triggers/int3.sql
@@ -4,13 +4,24 @@
 .nullvalue NULL
 */
 
---- Jogos que aconteceram numa determinada cidade
+--- Ordem cronológica de eventos de um jogo
 
-SELECT j.id, data, hora_de_inicio, e_visitada.nome as visitada, e_visitante.nome as visitante
-FROM Cidade c
-JOIN Recinto r on r.cidade = c.nome
-JOIN Equipa e_visitada on e_visitada.recinto = r.nome
-JOIN Jogo j on j.visitada = e_visitada.id
-JOIN Equipa e_visitante on e_visitante.id = j.visitante
-WHERE c.nome = 'Porto'
-ORDER BY data DESC, hora_de_inicio;
+select g.minuto, g.segundo, 'Golo' as evento, a.nome as 'Jogador', e.nome as 'Equipa'
+from Jogo j
+join Golo g on g.jogo = j.id
+join Atleta a on a.id = g.atleta
+join Equipa e on e.id = g.equipa
+where j.id = 227988
+union
+select p.minuto, p.segundo, 'Pausa Técnica' as evento, '' as 'Jogador', e.nome as 'Equipa'
+from Jogo j
+join PausaTecnica p on p.jogo = j.id
+join Equipa e on e.id = p.equipa
+where j.id = 227988
+union
+select i.minuto, i.segundo, i.tipo as evento, a.nome as 'Jogador', '' as 'Equipa'
+from Jogo j
+join Interrupcao i on i.jogo = j.id
+join Atleta a on a.id = i.atleta
+where j.id = 227988
+order by minuto, segundo;
